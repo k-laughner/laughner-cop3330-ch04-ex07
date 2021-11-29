7d15a9ffdcddc4a15dc427ddfdb6b3743b6b0d80
k-laughner <74153467+k-laughner@users.noreply.github.com>
2021-11-29 02:13:50 -0500

    Create main.h

---
+++ src/main.h
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Kaylee Laughner
+ */
+#include <iostream> 
+#include<bits/stdc++.h>
+#include <string>
+using namespace std;
+
+// prototypes
+ int getNumber(string num);
+ bool validateOp(string op);
+ bool validateOperands(string o);
+ double doCalc(string op, string op1, string op2);
+ void printResult(double result, string op, string op1, string op2);
